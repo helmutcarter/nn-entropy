bbf4b4b0a0cbfc0741f8a61a8155bcab32568a5a
helmutcarter <helmut.carter@gmail.com>
2025-04-08 11:02:01 -0400

    make more robust

--- assess_convergence.sh
+++ assess_convergence.sh
@@ -1,4 +1,7 @@
+date
+rm 2-methyl-hexane_unrestrained_convergence.csv
+cargo build --release
 for i in $(seq 10000 10000 1000000); do
     target/release/nn_entropy $i >> 2-methyl-hexane_unrestrained_convergence.csv
 done
-
+date
