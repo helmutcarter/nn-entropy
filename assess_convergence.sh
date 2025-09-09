0f8ebdad98db17fc571c6576171096217ddc49b9
helmutcarter <helmut.carter@gmail.com>
2025-09-09 12:38:42 -0400

    remove old testing files

--- assess_convergence.sh
+++
@@ -1,7 +0,0 @@
-date
-rm 2-methyl-hexane_unrestrained_convergence.csv
-cargo build --release
-for i in $(seq 10000 10000 1000000); do
-    target/release/nn_entropy $i >> 2-methyl-hexane_unrestrained_convergence.csv
-done
-date
